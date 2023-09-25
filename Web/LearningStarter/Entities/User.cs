4f4d4a4d386f93b82497076db045281162f6fb9f
Luke Norton <luke.norton@selu.edu>
2023-09-25 13:41:05 -0500

    testing

--- Web/LearningStarter/Entities/User.cs
+++ Web/LearningStarter/Entities/User.cs
@@ -37,6 +37,9 @@ public class UserGetDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string UserName { get; set; }
+
+    public string Password { get; set; }
+
 }
 
 public class UserEntityConfiguration : IEntityTypeConfiguration<User>
