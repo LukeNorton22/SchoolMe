b3aad50fb0e3b2695c754e5e9719665738e65207
Luke Norton <luke.norton@selu.edu>
2023-09-25 15:15:39 -0500

    Started creating entities Group and Message.

---
+++ Web/LearningStarter/Entities/Group.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace LearningStarter.Entities
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Description { get; set; } 
+        public List<User> Users { get; set; }
+        public List<Message> Messages { get; set; }
+
+    }
+
+    public class GroupCreateDto
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Description { get; set; }
+        public List<User> Users { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+
+    public class GroupEntityConfiguration : IEntityTypeConfiguration<Group>
+    {
+        public void Configure(EntityTypeBuilder<Group> builder)
+        {
+            builder.Property(x => x.Name)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+                .IsRequired();
+
+        }
+    }
+
+}
---
+++ Web/LearningStarter/Entities/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningStarter.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public Group Group { get; set; }
+        public User Sender {  get; set; }
+    }
+}
