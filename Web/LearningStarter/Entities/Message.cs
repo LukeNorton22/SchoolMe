7092a7e5595de7e77649ddfc20cc848a088f2957
Luke Norton <luke.norton@selu.edu>
2023-09-26 10:13:03 -0500

    Message entities

--- Web/LearningStarter/Entities/Message.cs
+++ Web/LearningStarter/Entities/Message.cs
@@ -1,4 +1,7 @@
-﻿namespace LearningStarter.Entities
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningStarter.Entities
 {
     public class Message
     {
@@ -6,5 +9,30 @@
         public string Content { get; set; }
         public Group Group { get; set; }
         public User Sender {  get; set; }
+    }
+
+    public class MessageGetDto
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public Group Group { get; set; }
+        public User Sender { get; set; }
+
+    }
+
+    public class MessageCreateDto
+    {
+        public string Content { get; set; }
+
+    }
+
+    public class MessageEntityTypeConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.ToTable("Message");
+        }
+
+        
     }
 }
