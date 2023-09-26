7198a8a6b42f3a98ae4e53dd7ae1548efca12dad
RobertTurner <mason.robertmason.robert1@gmail.com>
2023-09-26 13:58:06 -0500

    Commit 1 Entity and Controller for GroupMembers created

---
+++ Web/LearningStarter/Controllers/GroupMembersController.cs
@@ -0,0 +1,62 @@
+﻿using LearningStarter.Common;
+using LearningStarter.Data;
+using LearningStarter.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Linq;
+
+namespace LearningStarter.Controllers;
+[ApiController]
+[Route("api/GroupMembers")]
+
+    public class GroupMembersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public GroupMembersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var response = new Response();
+            var data = _dataContext
+                .Set<GroupMembers>()
+                .Select(GroupMembers => new GroupMembersGetDto
+                {
+                    Id = GroupMembers.Id,
+                    GroupId = GroupMembers.GroupId,
+                    UserId = GroupMembers.UserId,
+                })
+                .ToList();
+            response.Data = data;
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] GroupMembersCreateDto createDto)
+        {
+            var response = new Response();
+            var GroupMembersToCreate = new GroupMembers
+            {
+                GroupId = createDto.GroupId,
+                UserId = createDto.UserId,
+            };
+
+            _dataContext.Set<GroupMembers>().Add(GroupMembersToCreate);
+            _dataContext.SaveChanges();
+
+            var GroupMembersToReturn = new GroupMembersGetDto
+            {
+                Id = GroupMembersToCreate.Id,
+                GroupId = GroupMembersToCreate.GroupId,
+                UserId = GroupMembersToCreate.UserId,
+            };
+
+            response.Data = GroupMembersToReturn;
+            return Created("", response);
+
+        }
+            }
+    
+
---
+++ Web/LearningStarter/Entities/GroupMembers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningStarter.Entities;
+
+
+        public class GroupMembers
+        {
+            public int Id { get; set; }
+            public int GroupId { get; set; }
+            public int UserId { get; set; }
+        }
+
+        public class GroupMembersGetDto
+        {
+            public int Id { get; set; }
+            public int GroupId { get; set; }
+            public int UserId { get; set; }
+        }
+        public class GroupMembersCreateDto
+        {
+
+            public int GroupId { get; set; }
+            public int UserId { get; set; }
+        }
+public class GroupMembersUpdateDto
+{
+
+
+    public int GroupId { get; set; }
+    public int UserId { get; set; }
+}
+    public class GroupMembersEntityTypeConfiguration : IEntityTypeConfiguration<GroupMembers>
+    {
+        public void Configure(EntityTypeBuilder<GroupMembers> builder)
+        {
+            builder.ToTable("GroupMembers");
+        }
+    }
+
+
