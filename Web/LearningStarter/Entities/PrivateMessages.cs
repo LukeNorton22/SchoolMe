c6fa23b2c0980af9b75e4a5981ba75d870bad017
Luke Norton <luke.norton@selu.edu>
2023-09-28 08:41:53 -0500

    Created PrivateMessages entity and controller. Did not include any foreign keys yet until further notice

---
+++ Web/LearningStarter/Entities/PrivateMessages.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Net.Http;
+using System;
+
+
+namespace LearningStarter.Entities;
+
+public class PrivateMessages
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public string ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+}
+
+public class PrivateMessagesGetDto
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public string ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+}
+
+public class PrivateMessagesCreateDto
+{
+    public string Content { get; set; }
+    public string ImageUrl { get; set; }
+
+}
+
+public class PrivateMessagesUpdateDto
+{
+
+    public string Content { get; set; }
+    public string ImageUrl { get; set; }
+
+}
+
+public class PrivateMessagesEntityTypeConfiguration : IEntityTypeConfiguration<PrivateMessages>
+{
+    public void Configure(EntityTypeBuilder<PrivateMessages> builder)
+    {
+        builder.ToTable("PrivateMessages");
+    }
+
+}
