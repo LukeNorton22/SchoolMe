5322db2fa1f81c89543af72eeb72dc2ea7aa0457
RobertTurner <mason.robertmason.robert1@gmail.com>
2023-09-30 17:48:45 -0500

    Creating GroupUsers relationship

---
+++ Web/LearningStarter/Entities/GroupUsers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningStarter.Entities
+{
+    public class GroupUser
+    {
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+
+            }
+
+    public class GroupUserGetDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class GroupUserEntityTypeConfiguration : IEntityTypeConfiguration<GroupUser>
+    {
+        public void Configure(EntityTypeBuilder<GroupUser> builder)
+        {
+            builder.ToTable("GroupUsers");
+
+            builder.HasOne(x => x.Group)
+                .WithMany(x => x.Users);
+
+            builder.HasOne(x => x.User)
+                .WithMany();
+        }
+    }
+
